096992c7ee2190b36e3da4daf4a5215437fb9f4f
Andre Ibarra Perez <RayAsm1@hotmail.com>
2020-02-05 21:04:48 -0600

    V1

---
+++ TuringComplementoBinario/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace TuringComplementoBinario
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCadena = new System.Windows.Forms.TextBox();
+            this.txtComplemento = new System.Windows.Forms.TextBox();
+            this.lblCadena = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtCadena
+            // 
+            this.txtCadena.Location = new System.Drawing.Point(69, 36);
+            this.txtCadena.MaxLength = 20;
+            this.txtCadena.Name = "txtCadena";
+            this.txtCadena.Size = new System.Drawing.Size(100, 20);
+            this.txtCadena.TabIndex = 0;
+            this.txtCadena.TextChanged += new System.EventHandler(this.txtCadena_TextChanged);
+            this.txtCadena.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtCadena_KeyPress);
+            // 
+            // txtComplemento
+            // 
+            this.txtComplemento.Location = new System.Drawing.Point(268, 36);
+            this.txtComplemento.MaxLength = 20;
+            this.txtComplemento.Name = "txtComplemento";
+            this.txtComplemento.Size = new System.Drawing.Size(100, 20);
+            this.txtComplemento.TabIndex = 1;
+            this.txtComplemento.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtComplemento_KeyPress);
+            // 
+            // lblCadena
+            // 
+            this.lblCadena.AutoSize = true;
+            this.lblCadena.Location = new System.Drawing.Point(17, 39);
+            this.lblCadena.Name = "lblCadena";
+            this.lblCadena.Size = new System.Drawing.Size(47, 13);
+            this.lblCadena.TabIndex = 2;
+            this.lblCadena.Text = "Cadena:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(192, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Complemento:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(158, 71);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Calcular complemento";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(0, 8);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(390, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Complemento de un número binario con Máquina de Turing";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 116);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblCadena);
+            this.Controls.Add(this.txtComplemento);
+            this.Controls.Add(this.txtCadena);
+            this.Name = "Form1";
+            this.Text = "TuringComplementoBinario";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCadena;
+        private System.Windows.Forms.TextBox txtComplemento;
+        private System.Windows.Forms.Label lblCadena;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ TuringComplementoBinario/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TuringComplementoBinario
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtCadena_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+        private void SoloNumeros(KeyPressEventArgs e)
+        {
+            //Para obligar a que sólo se introduzcan números 
+            if (Char.IsDigit(e.KeyChar))
+            {
+                
+                if(e.KeyChar >= '2' && e.KeyChar <= '9')
+                {
+                    e.Handled = true;
+                }
+                else
+                {
+                    e.Handled = false;
+                }
+            }
+            else if (Char.IsControl(e.KeyChar)) //permitir teclas de control como retroceso 
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                //el resto de teclas pulsadas se desactivan 
+                e.Handled = true;
+            }
+        }
+
+        private void txtCadena_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            SoloNumeros(e);
+        }
+
+        private void txtComplemento_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            SoloNumeros(e);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            decodificar(txtCadena.Text);
+        }
+
+
+        //Declaración de las variables
+        public static String cadena, cadena2, aux, reader;
+        public static int longitud;
+        public String complemento = "";
+        public String[] arreglo;
+        public int apuntador = 1;
+
+        //Metodo decodificar se encarga de crear el arreglo que contendrá la cadena
+        public void decodificar(String dec)
+        {
+            cadena = txtCadena.Text;
+            cadena2 = "#" + cadena + "#";
+            longitud = cadena2.Length;
+            arreglo = new String[longitud];
+            for (int i = 0; i < longitud; i++)
+            {
+                arreglo[i] = "" + cadena2[i];
+            }
+
+            e0();
+        }
+
+        //El metodo e0 representa al estado 0, el estado inicial
+        public void e0()
+        {
+            aux = arreglo[apuntador];
+            if (aux.Equals("0"))
+            {
+                arreglo[apuntador] = "1";
+                moverDerecha();
+                e1();
+            }
+            else if (aux.Equals("1"))
+            {
+                arreglo[apuntador] = "0";
+                moverDerecha();
+                e1();
+            }
+            else if (aux.Equals("#"))
+            {
+                arreglo[apuntador] = "#";
+                moverIzquierda();
+                MessageBox.Show("Cadena vacia");
+            }
+            else
+            {
+                rechazar(aux);
+            }
+
+        }
+
+        //El metodo e1 representa al estado 1
+        public void e1()
+        {
+            aux = arreglo[apuntador];
+            if (aux.Equals("0"))
+            {
+                arreglo[apuntador] = "1";
+                moverDerecha();
+                e1();
+            }
+            else if (aux.Equals("1"))
+            {
+                arreglo[apuntador] = "0";
+                moverDerecha();
+                e1();
+            }
+            else if (aux.Equals("#"))
+            {
+                arreglo[apuntador] = "#";
+                moverIzquierda();
+                e2();
+            }
+            else
+            {
+                rechazar(aux);
+            }
+        }
+
+        //El metodo e2 representa al estado 2
+        public void e2()
+        {
+            aux = arreglo[apuntador];
+            if (aux.Equals("0"))
+            {
+                arreglo[apuntador] = "0";
+                moverIzquierda();
+                e2();
+            }
+            else if (aux.Equals("1"))
+            {
+                arreglo[apuntador] = "1";
+                moverIzquierda();
+                e2();
+            }
+            else if (aux.Equals("#"))
+            {
+                arreglo[apuntador] = "#";
+                moverDerecha();
+                e3();
+            }
+            else
+            {
+                rechazar(aux);
+            }
+        }
+
+        //El metodo e3 representa al estado 3, el estado de aceptación
+        public void e3()
+        {
+
+            for (int i = 1; i < longitud - 1; i++)
+            {
+                complemento = complemento + arreglo[i];
+            }
+            txtComplemento.Text = complemento;
+        }
+
+
+
+        //Mueve el puntero de la MT a la derecha
+        public void moverDerecha()
+        {
+            apuntador++;
+        }
+
+        //Mueve el puntero de la MT a la izquierda
+        public void moverIzquierda()
+        {
+            apuntador = apuntador - 1;
+        }
+
+        //Si entra en este estado, significa que se encontró un caracter no valido
+        public void rechazar(String noval)
+        {
+            MessageBox.Show("Caracter no valido: " + noval);
+        }
+
+
+    }
+}
